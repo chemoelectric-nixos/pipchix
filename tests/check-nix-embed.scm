7673db090a9fb225c456d57df1be91f2fb4b939f
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2025-11-20 10:03:56 -0600

    Add a test for nix-embed

---
+++ tests/check-nix-embed.scm
@@ -0,0 +1,5 @@
+(define ast100 (nix-embed "let a = 4; b = a + 5; in b"))
+(set! result "")
+(output-nix-abstract-syntax-tree ast100 fill-result)
+(unless (string=? result "let a = 4; b = a + 5; in b")
+  (exit FAILURE))
