19868a03e24803ced6df8e3f93033bccaa903b59
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2025-11-23 17:16:31 -0600

    The test template has many of the tests we already had written. I have left out so far the tests for ‘foo?’ procedures. Those will be filled in.

--- tests/check-nix-embed.scm
+++
@@ -1,7 +0,0 @@
-(define ast100 (nix-embed "let a = 4; b = a + 5; in b"))
-(set! result "")
-(output-nix-abstract-syntax-tree ast100 fill-result)
-(unless (string=? result "let a = 4; b = a + 5; in b")
-  (exit FAIL))
-
-(exit PASS)
